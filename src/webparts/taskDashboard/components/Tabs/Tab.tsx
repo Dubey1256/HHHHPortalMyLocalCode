e2ded95e1de0212bee601072e2925f7f03a35b77
Santoshk00 <sk12211637@gmail.com>
2022-12-05 18:00:29 +0530

    TaskDasboard

---
+++ src/webparts/taskDashboard/components/Tabs/Tab.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './styles.css';
+type Props = {
+  title: string
+}
+
+const Tab = ({ children }:any) => {
+  return <div className="nav nav-tabs nav nav-pills " >{children}</div>
+}
+
+export default Tab
