60f5c9a232c0f35b5d028f7bceda963c3dbcdbbc
Trivedi85 <ranutrivedi220@gmail.com>
2023-02-13 16:33:26 +0530

    New changes

--- src/webparts/servicePortfolio/components/componentTable.tsx
+++ src/webparts/servicePortfolio/components/componentTable.tsx
@@ -35,6 +35,7 @@ var ComponentsDataCopy: any = [];
 var SubComponentsDataCopy: any = [];
 var FeatureDataCopy: any = [];
 var array: any = [];
+var serachTitle: any = '';
 function ComponentTable(SelectedProp: any) {
 
     const [maidataBackup, setmaidataBackup] = React.useState([])
@@ -141,7 +142,7 @@ function ComponentTable(SelectedProp: any) {
         // setState(state)
     }
     const Clearitem = () => {
-      
+
         maidataBackup.forEach(function (val: any) {
             val.show = false;
             if (val.childs != undefined) {
@@ -161,17 +162,17 @@ function ComponentTable(SelectedProp: any) {
                 })
             }
         })
-        filterItems.forEach(function(itemm:any){
+        filterItems.forEach(function (itemm: any) {
             itemm.Selected = false;
         })
-       
+
         setSubComponentsData(SubComponentsDataCopy);
         setFeatureData(FeatureDataCopy);
         setmaidataBackup(ComponentsDataCopy)
         setShowSelectdSmartfilter([])
-      
+
         setState([])
-        
+
 
         setData(maidataBackup)
         // const { checked } = e.target;
@@ -703,7 +704,7 @@ function ComponentTable(SelectedProp: any) {
     };
     const handleOpen = (item: any) => {
         item.show = item.show = item.show == true ? false : true;
-        setData(maidataBackup => ([...maidataBackup]));
+        setData(data => ([...data]));
     };
     const handleOpenAll = () => {
         var Isshow1: any = Isshow == true ? false : true;
@@ -792,10 +793,10 @@ function ComponentTable(SelectedProp: any) {
     };
     var getHighlightdata = function (item: any, searchTerms: any) {
         var keywordList = [];
-        if (searchTerms != undefined && searchTerms != '') {
-            keywordList = stringToArray(searchTerms);
+        if (serachTitle != undefined && serachTitle != '') {
+            keywordList = stringToArray(serachTitle);
         } else {
-            keywordList = stringToArray(searchTerms);
+            keywordList = stringToArray(serachTitle);
         }
         var pattern: any = getRegexPattern(keywordList);
         //let Title :any =(...item.Title)
@@ -828,12 +829,13 @@ function ComponentTable(SelectedProp: any) {
     }
     let handleChange1 = (e: { target: { value: string; }; }, titleName: any) => {
         setSearch(e.target.value.toLowerCase());
+        serachTitle = e.target.value.toLowerCase();
         var Title = titleName;
 
-        var AllFilteredTagNews:any = [];
-        var childData:any = [];
-        var subChild:any = [];
-        var subChild2:any = [];
+        var AllFilteredTagNews: any = [];
+        var childData: any = [];
+        var subChild: any = [];
+        var subChild2: any = [];
         AllFilteredTagNews.forEach(function (val: any) {
             val.Child = []
             if (val.childs != undefined) {
@@ -842,9 +844,9 @@ function ComponentTable(SelectedProp: any) {
                     if (type.childs != undefined) {
                         type.childs.forEach(function (value: any) {
                             value.Child = []
-                            if(value.childs != undefined){
-                                value.childs.forEach(function(last:any){
-                                    last.Child=[]
+                            if (value.childs != undefined) {
+                                value.childs.forEach(function (last: any) {
+                                    last.Child = []
 
                                 })
                             }
@@ -861,10 +863,10 @@ function ComponentTable(SelectedProp: any) {
                 item.isSearch = true;
                 item.show = false;
                 item.flag = (getSearchTermAvialable1(searchTerms, item, Title));
-                if(item.flag == true){
+                if (!isItemExistsNew(AllFilteredTagNews ,item) && item.flag === true) {
                     AllFilteredTagNews.push(item)
                 }
-                
+
                 if (item.childs != undefined && item.childs.length > 0) {
                     $.each(item.childs, function (parentIndex: any, child1: any) {
                         child1.flag = false;
@@ -875,8 +877,11 @@ function ComponentTable(SelectedProp: any) {
                             data[pareIndex].flag = true;
                             item.childs[parentIndex].show = true;
                             data[pareIndex].show = true;
+                            if (!isItemExistsNew(AllFilteredTagNews ,item)) {
+                                AllFilteredTagNews.push(item)
+                            }
                             childData.push(child1)
-                           
+
                         }
                         if (child1.childs != undefined && child1.childs.length > 0) {
                             $.each(child1.childs, function (index: any, subchild: any) {
@@ -890,8 +895,13 @@ function ComponentTable(SelectedProp: any) {
                                     item.childs[parentIndex].show = true;
                                     data[pareIndex].flag = true;
                                     data[pareIndex].show = true;
+                                    if (!isItemExistsNew(AllFilteredTagNews ,item)) {
+                                        AllFilteredTagNews.push(item)
+                                    }
+                                    if (!isItemExistsNew(childData ,child1) )
+                                        childData.push(child1)
                                     subChild.push(subchild)
-                                    
+
                                 }
                                 if (subchild.childs != undefined && subchild.childs.length > 0) {
                                     $.each(subchild.childs, function (childindex: any, subchilds: any) {
@@ -908,8 +918,15 @@ function ComponentTable(SelectedProp: any) {
                                             item.childs[parentIndex].show = true;
                                             data[pareIndex].flag = true;
                                             data[pareIndex].show = true;
+                                            if (!isItemExistsNew(AllFilteredTagNews ,item) ) {
+                                                AllFilteredTagNews.push(item)
+                                            }
+                                            if (!isItemExistsNew(childData ,child1) )
+                                                childData.push(child1)
+                                                if (!isItemExistsNew(subChild ,subChild) )
+                                                subChild.push(subChild)
                                             subChild2.push(subchilds)
-                                            
+
                                         }
                                     })
                                 }
@@ -918,7 +935,7 @@ function ComponentTable(SelectedProp: any) {
 
                     })
                 }
-                
+
             })
             const CData = AllFilteredTagNews.filter((val: any, id: any, array: any) => {
                 return array.indexOf(val) == id;
@@ -929,7 +946,8 @@ function ComponentTable(SelectedProp: any) {
             const FData = subChild.filter((val: any, id: any, array: any) => {
                 return array.indexOf(val) == id;
             })
-           
+
+
             setSubComponentsData(SData);
             setFeatureData(FData);
             setComponentsData(CData);
@@ -1896,7 +1914,7 @@ function ComponentTable(SelectedProp: any) {
                 })
             }
         })
-       
+
         var AllTaggedTask: any = [];
         $.each(SelectedList, function (index: any, item: any) {
             $.each(AllTaskData1, function (index: any, task: any) {
@@ -2072,7 +2090,7 @@ function ComponentTable(SelectedProp: any) {
     const ShowAllChildsPlus = (item: any) => {
         if (item.childs?.length > 0) {
             item.Isexpend = true;
-            item.show =false;
+            item.show = false;
             handleOpen(item);
             item.childs.forEach((child: any) => {
                 child.flag = true;
@@ -2442,6 +2460,9 @@ function ComponentTable(SelectedProp: any) {
                                         <label>
                                             {FeatureData.length} of {FeatureData.length} Features
                                         </label>
+                                        {/* <label>
+                                            {FeatureData.length} of {FeatureData.length} Tasks
+                                        </label> */}
                                         <span className="g-search">
                                             <input type="text" className="searchbox_height full_width" id="globalSearch" placeholder="search all" />
                                             <span className="gsearch-btn" ><i><FaSearch /></i></span>
@@ -2502,8 +2523,8 @@ function ComponentTable(SelectedProp: any) {
                                                 <thead>
                                                     <tr>
                                                         <th style={{ width: "2%" }}>
-                                                            <div className="smart-relative sign hreflink" onClick={() => handleOpenAll()} >{Isshow ? <img src="https://hhhhteams.sharepoint.com/sites/HHHH/SP/SiteCollectionImages/ICONS/24/list-icon.png" />
-                                                                : <img src="https://hhhhteams.sharepoint.com/sites/HHHH/SP/SiteCollectionImages/ICONS/24/right-list-icon.png" />}
+                                                            <div className="smart-relative sign hreflink" onClick={() => handleOpenAll()} >{Isshow ? <img src={(IsUpdated != undefined && IsUpdated.toLowerCase().indexOf('service') > -1) ? "https://hhhhteams.sharepoint.com/sites/HHHH/SP/SiteCollectionImages/ICONS/Service_Icons/Downarrowicon-green.png" : 'https://hhhhteams.sharepoint.com/sites/HHHH/SP/SiteCollectionImages/ICONS/24/list-icon.png'} />
+                                                                : <img src={(IsUpdated != undefined && IsUpdated.toLowerCase().indexOf('service') > -1) ? "https://hhhhteams.sharepoint.com/sites/HHHH/SP/SiteCollectionImages/ICONS/Service_Icons/Rightarrowicon-green.png" : "https://hhhhteams.sharepoint.com/sites/HHHH/SP/SiteCollectionImages/ICONS/24/right-list-icon.png"} />}
                                                             </div>
                                                         </th>
                                                         <th style={{ width: "2%" }}>
@@ -2836,7 +2857,9 @@ function ComponentTable(SelectedProp: any) {
                                                                                                             <td style={{ width: "10%" }}>{childitem.ItemRank}</td>
                                                                                                             <td style={{ width: "10%" }}>{childitem.DueDate}</td>
                                                                                                             <td style={{ width: "3%" }}>{childitem.siteType != "Master Tasks" && <a onClick={(e) => EditData(e, childitem)} data-bs-toggle="tooltip" data-bs-placement="auto" title="Click To Edit Timesheet"><img style={{ width: "22px" }} src="https://hhhhteams.sharepoint.com/sites/HHHH/SP/SiteCollectionImages/ICONS/24/clock-gray.png"></img></a>}</td>
-                                                                                                            <td style={{ width: "3%" }}><a data-bs-toggle="tooltip" data-bs-placement="auto" title="Edit">{childitem.siteType == "Master Tasks" && <img src="https://hhhhteams.sharepoint.com/_layouts/images/edititem.gif" onClick={(e) => EditComponentPopup(childitem)} />}
+                                                                                                            <td style={{ width: "3%" }}><a data-bs-toggle="tooltip" data-bs-placement="auto" title="Edit">
+                                                                                                                {childitem.siteType == "Master Tasks" &&
+                                                                                                                    <img src="https://hhhhteams.sharepoint.com/_layouts/images/edititem.gif" onClick={(e) => EditComponentPopup(childitem)} />}
                                                                                                                 {childitem.siteType != "Master Tasks" && <img data-bs-toggle="tooltip" data-bs-placement="auto" title="Edit" src="https://hhhhteams.sharepoint.com/_layouts/images/edititem.gif" onClick={(e) => EditItemTaskPopup(childitem)} />}</a></td>
                                                                                                         </tr>
                                                                                                     </table>
@@ -2959,8 +2982,10 @@ function ComponentTable(SelectedProp: any) {
                                                                                                                                     <td style={{ width: "6%" }}>{childinew.PercentComplete}</td>
                                                                                                                                     <td style={{ width: "10%" }}>{childinew.ItemRank}</td>
                                                                                                                                     <td style={{ width: "10%" }}>{childinew.DueDate}</td>
-                                                                                                                                    <td style={{ width: "3%" }}>{childinew.siteType != "Master Tasks" && <a onClick={(e) => EditData(e, childinew)} data-bs-toggle="tooltip" data-bs-placement="auto" title="Click To Edit Timesheet"><img style={{ width: "22px" }} data-bs-toggle="tooltip" data-bs-placement="bottom" title="Click To Edit Timesheet" src="https://hhhhteams.sharepoint.com/sites/HHHH/SP/SiteCollectionImages/ICONS/24/clock-gray.png"></img></a>}</td>
-                                                                                                                                    <td style={{ width: "3%" }}>{childinew.siteType == "Master Tasks" && <a data-bs-toggle="tooltip" data-bs-placement="auto" title="Edit">   <img src="https://hhhhteams.sharepoint.com/_layouts/images/edititem.gif" onClick={(e) => EditComponentPopup(childinew)} /></a>}</td>
+                                                                                                                                    <td style={{ width: "3%" }}>{childinew.siteType != "Master Tasks" && <a onClick={(e) => EditData(e, childinew)} data-bs-toggle="tooltip" data-bs-placement="auto" title="Click To Edit Timesheet"><img style={{ width: "22px" }} data-bs-toggle="tooltip" data-bs-placement="bottom" title="Click To Edit Timesheet" src="https://hhhhteams.sharepoint.com/sites/HHHH/SP/SiteCollectionImages/ICONS/24/clock-gray.png"></img></a>}
+                                                                                                                                    </td>
+                                                                                                                                    <td style={{ width: "3%" }}> {childinew.siteType != "Master Tasks" && <img data-bs-toggle="tooltip" data-bs-placement="auto" title="Edit" src="https://hhhhteams.sharepoint.com/_layouts/images/edititem.gif" onClick={(e) => EditItemTaskPopup(childinew)} />}
+                                                                                                                                        {childinew.siteType == "Master Tasks" && <a data-bs-toggle="tooltip" data-bs-placement="auto" title="Edit">   <img src="https://hhhhteams.sharepoint.com/_layouts/images/edititem.gif" onClick={(e) => EditComponentPopup(childinew)} /></a>}</td>
                                                                                                                                 </tr>
                                                                                                                             </table>
                                                                                                                         </td>
@@ -3078,7 +3103,7 @@ function ComponentTable(SelectedProp: any) {
                                                                                                                                                         <td style={{ width: "10%" }}>{subchilditem.ItemRank}</td>
                                                                                                                                                         <td style={{ width: "10%" }}>{subchilditem.DueDate}</td>
                                                                                                                                                         <td style={{ width: "3%" }}>{subchilditem.siteType != "Master Tasks" && <a onClick={(e) => EditData(e, subchilditem)} data-bs-toggle="tooltip" data-bs-placement="auto" title="Click To Edit Timesheet"><img style={{ width: "22px" }} src="https://hhhhteams.sharepoint.com/sites/HHHH/SP/SiteCollectionImages/ICONS/24/clock-gray.png"></img></a>}</td>
-                                                                                                                                                        <td style={{ width: "3%" }}></td>
+                                                                                                                                                        <td style={{ width: "3%" }}> {subchilditem.siteType != "Master Tasks" && <img data-bs-toggle="tooltip" data-bs-placement="auto" title="Edit" src="https://hhhhteams.sharepoint.com/_layouts/images/edititem.gif" onClick={(e) => EditItemTaskPopup(subchilditem)} ></img>}</td>
                                                                                                                                                     </tr>
                                                                                                                                                 </table>
                                                                                                                                             </td>
