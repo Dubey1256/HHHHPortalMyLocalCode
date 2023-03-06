015ad7f061a1ba5cdbe4235c97e9fabc1f110780
abhi98989 <abhishek09019@gmail.com>
2023-03-06 19:17:11 +0530

    changes project overview 6 march

--- src/webparts/projectManagement/components/TagTaskToProjectPopup.tsx
+++ src/webparts/projectManagement/components/TagTaskToProjectPopup.tsx
@@ -53,7 +53,7 @@ const TagTaskToProjectPopup = (props: any) => {
         siteConfig = smartmeta;
         LoadAllSiteTasks();
     }
- 
+
     const loadAdminConfigurations = function () {
 
         var CurrentSiteType = ''
@@ -71,6 +71,31 @@ const TagTaskToProjectPopup = (props: any) => {
                 function (error) {
 
                 });
+    }
+    const checkTaggedService = (item: any) => {
+        let tagged = false
+        if(item!=undefined){
+           
+            if (props?.projectItem?.ServicesId.includes(item)) {
+                tagged = true;
+            } else {
+                tagged = false;
+            }
+        
+        }
+        return tagged;
+    }
+    const checkTaggedComponent = (item: any) => {
+        let tagged = false
+           if(item!=undefined) {
+        if (props?.projectItem?.ComponentId.includes(item)) {
+            tagged = true;
+        } else {
+            tagged = false;
+        }   
+    }
+        return tagged;
+
     }
     const LoadAllSiteTasks = function () {
         loadAdminConfigurations();
@@ -94,61 +119,77 @@ const TagTaskToProjectPopup = (props: any) => {
                     .get();
                 arraycount++;
                 smartmeta.map((items: any) => {
-                    items.AllTeamMember = []
-                    items.siteType = config.Title;
-                    if (items?.Project?.Id == props?.projectId) {
-                        items.selected = true;
-                    } else {
-                        items.selected = false;
-                    }
-                    items.listId = config.listId;
-                    items.siteUrl = config.siteUrl.Url;
-                    items.PercentComplete = (items.PercentComplete * 100).toFixed(0);
-                    if (items.Component != undefined && items.Component.results != undefined && items.Component.results.length > 0) {
-                        items['Portfoliotype'] = 'Component';
-                    }
-                    if (items.Services != undefined && items.Services.results && items.Services.results.length > 0) {
-                        items['Portfoliotype'] = 'Service';
-                    }
-                    if (DataSiteIcon != undefined) {
-                        if (config.Title == "DRR" || config.Title == "Gender" || config.Title == "KathaBeck") {
-                            items['siteIcon'] = config.Item_x005F_x0020_Cover.Url
-                        } else {
-                            DataSiteIcon.map((site: any) => {
-                                if (site.Site == items.siteType) {
-                                    items['siteIcon'] = site.SiteIcon
-                                }
-                            })
+                    try {
+                        let toBePushed = false;
+                        if ( props?.projectItem?.ServicesId?.length > 0 ) {
+                            toBePushed = checkTaggedService(items?.ServicesId?.[0])
                         }
-
-                    }
-                    items.CreatedDis = items?.Created != null ? moment(items.Created).format('DD/MM/YYYY') : ""
-                    items.componentString = items.Component != undefined && items.Component != undefined && items.Component.length > 0 ? getComponentasString(items.Component) : '';
-                    items.Shareweb_x0020_ID = globalCommon.getTaskId(items);
-                    if (items.Team_x0020_Members != undefined) {
-                        items.Team_x0020_Members.map((taskUser: any) => {
-                            var newuserdata: any = {};
-
-                            AllUser.map((user: any) => {
-                                if (user.AssingedToUserId == taskUser.Id) {
-
-                                    newuserdata['useimageurl'] = user?.Item_x0020_Cover?.Url;
-                                    newuserdata['Suffix'] = user.Suffix;
-                                    newuserdata['Title'] = user.Title;
-                                    newuserdata['UserId'] = user.AssingedToUserId;
-                                    items['Usertitlename'] = user.Title;
-                                }
-
+                        if (props?.projectItem?.ComponentId?.length > 0 ) {
+                            toBePushed = checkTaggedComponent(items?.ComponentId?.[0])
+                        }
+                        if(toBePushed){
+                        items.AllTeamMember = []
+                        items.siteType = config.Title;
+                        if (items?.Project?.Id == props?.projectId) {
+                            items.selected = true;
+                        } else {
+                            items.selected = false;
+                        }
+                        items.listId = config.listId;
+                        items.siteUrl = config.siteUrl.Url;
+                        items.PercentComplete = (items.PercentComplete * 100).toFixed(0);
+                        if (items?.Component?.results?.length > 0) {
+                            items['Portfoliotype'] = 'Component';
+                        }
+                        if (items?.Services?.results?.length > 0) {
+                            items['Portfoliotype'] = 'Service';
+                        }
+                        if (DataSiteIcon != undefined) {
+                            if (config.Title == "DRR" || config.Title == "Gender" || config.Title == "KathaBeck") {
+                                items['siteIcon'] = config.Item_x005F_x0020_Cover.Url
+                            } else {
+                                DataSiteIcon.map((site: any) => {
+                                    if (site.Site == items.siteType) {
+                                        items['siteIcon'] = site.SiteIcon
+                                    }
+                                })
+                            }
+    
+                        }
+                        items.CreatedDis = items?.Created != null ? moment(items.Created).format('DD/MM/YYYY') : ""
+                        items.componentString = items.Component != undefined && items.Component != undefined && items.Component.length > 0 ? getComponentasString(items.Component) : '';
+                        items.Shareweb_x0020_ID = globalCommon.getTaskId(items);
+                        if (items.Team_x0020_Members != undefined) {
+                            items.Team_x0020_Members.map((taskUser: any) => {
+                                var newuserdata: any = {};
+    
+                                AllUser.map((user: any) => {
+                                    if (user.AssingedToUserId == taskUser.Id) {
+    
+                                        newuserdata['useimageurl'] = user?.Item_x0020_Cover?.Url;
+                                        newuserdata['Suffix'] = user.Suffix;
+                                        newuserdata['Title'] = user.Title;
+                                        newuserdata['UserId'] = user.AssingedToUserId;
+                                        items['Usertitlename'] = user.Title;
+                                    }
+    
+                                })
+                                items.AllTeamMember.push(newuserdata);
                             })
-                            items.AllTeamMember.push(newuserdata);
-                        })
-
+    
+                        }
+                      
+                            AllTask.push(items)
+                        }
+                    } catch (error) {
+                        console.warn('tagTaskError',error)
                     }
-                    AllTask.push(items)
+        
                 })
 
                 if (arraycount === 17) {
                     setAllTasks(AllTask)
+                    setSearchedAllTasks(AllTask)
 
                     // setmaidataBackup(AllTask)
                     // showProgressHide();
@@ -185,13 +226,10 @@ const TagTaskToProjectPopup = (props: any) => {
         })
     }
     const [searchText, setSearchText] = useState("");
-    const onSearchText = (e: any) => {
-        setSearchText(e.target.value);
-
-    };
+  
     const clearSearch = () => {
         setSearchText('');
-        setSearchedAllTasks([])
+        setSearchedAllTasks(AllTasks)
         setselectAll(false)
     }
     const searchTaskToTag = () => {
@@ -243,9 +281,8 @@ const TagTaskToProjectPopup = (props: any) => {
     }
 
     const handleKeyDown = (event: any) => {
-        if (event.key === 'Enter') {
-            searchTaskToTag();
-        }
+        setSearchText(event.target.value);
+        searchTaskToTag();
     }
     const selectRow = (item: any, ind: any) => {
         let selectedItems = 0;
@@ -304,11 +341,11 @@ const TagTaskToProjectPopup = (props: any) => {
                         <div className='row'>
                             <div className='col-sm-6'>
                                 <div className="input-group">
-                                    <input className="form-control " type="text" value={searchText} onKeyDown={handleKeyDown} onChange={onSearchText} placeholder="Search" aria-label="Search" />
+                                    <input className="form-control " type="text" value={searchText} onChange={handleKeyDown} placeholder="Search" aria-label="Search" />
                                     <span className="input-group-text" onClick={() => { searchTaskToTag() }}><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 48 48" fill="none">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M31.0138 7.06731C33.8354 7.61202 36.1852 8.86678 37.9071 10.7481C39.8647 12.8866 40.832 15.2084 40.979 18.1218C41.0896 20.3107 40.7731 21.8402 39.7795 23.9182C39.1457 25.2435 38.8458 25.6499 37.5723 26.9085C35.1834 29.2697 33.1175 30.1768 29.783 30.3289C27.9988 30.4101 27.6948 30.3806 26.3472 29.9939C24.6941 29.5197 23.8671 29.1402 22.7405 28.3386C22.3186 28.0385 21.9187 27.7929 21.8519 27.7929C21.7851 27.7929 18.7525 30.7738 15.1127 34.4173C8.73245 40.8041 8.47514 41.04 7.93518 40.9963C7.15937 40.9331 6.78471 40.3003 7.12757 39.6316C7.25623 39.3804 10.2498 36.2724 13.78 32.7251C17.3101 29.1776 20.1984 26.2245 20.1984 26.1623C20.1984 26.1003 19.9343 25.6607 19.6115 25.1856C17.4873 22.0598 17.0424 17.9103 18.4541 14.3929C19.8012 11.0364 22.4973 8.58667 26.0904 7.45461C27.2002 7.10508 30.0487 6.88084 31.0138 7.06731ZM27.5326 9.2402C26.2441 9.47559 24.1717 10.4672 23.1928 11.3167C20.2179 13.8984 19.0413 17.6838 20.0893 21.3025C21.0407 24.5876 23.6698 27.1581 26.9782 28.0375C28.2549 28.3769 30.5384 28.3442 31.8541 27.9679C35.2193 27.0055 37.9175 24.2229 38.6949 20.9132C39.1038 19.1722 38.9464 16.9223 38.3009 15.2809C36.5751 10.8934 32.1303 8.40004 27.5326 9.2402Z" fill="#333333" />
                                     </svg></span>
-                                    {searchText?.length > 0 ? <span className='searchclear' onClick={clearSearch} >×</span> : ''}
+                                    {searchText?.length > 0 ? <span className={searchText?.length > 0?'searchclear':'searchclear'} onClick={clearSearch} >×</span> : ''}
                                 </div>
 
                             </div>
@@ -316,7 +353,7 @@ const TagTaskToProjectPopup = (props: any) => {
                                 <div className="col-sm-12">
                                     <div className="tbl-header">
                                         <table className="table">
-                                            <thead>
+                                            <thead> 
                                                 <tr>
                                                     <th style={{ width: "3%" }}><input type="checkbox" id="isActive" checked={selectAll} defaultChecked={selectAll} onChange={() => selectAllFiltered(selectAll)} /></th>
                                                     <th style={{ width: "5%" }}>Site</th>
