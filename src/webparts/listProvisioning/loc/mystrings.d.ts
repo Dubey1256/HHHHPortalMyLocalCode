14f734e59864787b2bd8ba56f8b47c2fb533b68f
chetansingh619 <chetansingh619@gmail.com>
2023-05-26 11:31:23 +0530

    merge new changes and list provisioning WP

---
+++ src/webparts/listProvisioning/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IListProvisioningWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ListProvisioningWebPartStrings' {
+  const strings: IListProvisioningWebPartStrings;
+  export = strings;
+}
