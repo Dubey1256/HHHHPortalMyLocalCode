32e5b4515bc9783ca796d1009e45fc2ac7ced729
abhi98989 <abhishek09019@gmail.com>
2023-06-26 14:42:39 +0530

    project overview fix 26  june

--- src/webparts/testWebPart/components/TestWebPart.tsx
+++ src/webparts/testWebPart/components/TestWebPart.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import styles from './TestWebPart.module.scss';
 import { ITestWebPartProps } from './ITestWebPartProps';
 import { escape } from '@microsoft/sp-lodash-subset';
-import DisplayDetails from './AccountView';
+// import DisplayDetails from './AccountView';
 
 export default class TestWebPart extends React.Component<ITestWebPartProps, {}> {
   public render(): React.ReactElement<ITestWebPartProps> {
@@ -15,7 +15,7 @@ export default class TestWebPart extends React.Component<ITestWebPartProps, {}> 
     } = this.props;
 
     return (
-      <DisplayDetails />
+     <span></span>
     );
   }
 }
