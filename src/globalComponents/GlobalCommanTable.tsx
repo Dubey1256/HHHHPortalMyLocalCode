dfc6b4ff734e1f1943267f8d1e45b0351ea48fc2
abhi98989 <abhishek09019@gmail.com>
2023-06-28 13:14:45 +0530

    project overview fix 28  june

--- src/globalComponents/GlobalCommanTable.tsx
+++ src/globalComponents/GlobalCommanTable.tsx
@@ -286,7 +286,7 @@ const GlobalCommanTable = (items: any) => {
                 <span className="toolbox mx-auto">
                     {showTeamMemberOnCheck === true ? <span><a className="teamIcon" onClick={() => ShowTeamFunc()}><span title="Create Teams Group" className="svg__iconbox svg__icon--team teamIcon"></span></a>
                     </span> : <span><a className="teamIcon"><span title="Create Teams Group" style={{ backgroundColor: "gray" }} className="svg__iconbox svg__icon--team teamIcon"></span></a></span>}
-                    {table?.getFilteredRowModel()?.rows?.length> 0 ? <span>
+                    {table?.getSelectedRowModel()?.rows?.length> 0 ? <span>
                         <a onClick={() => openTaskAndPortfolioMulti()} className="openWebIcon"><span className="svg__iconbox svg__icon--openWeb"></span></a>
                     </span> : <span><a className="openWebIcon"><span className="svg__iconbox svg__icon--openWeb" style={{ backgroundColor: "gray" }}></span></a></span>}
                     <a className='excal' onClick={() => downloadExcel(excelDatas, "Task-User-Management")}><RiFileExcel2Fill /></a>
