1d5cc420c504854d9d24c0b226a270d99b243ed9
vivekanand2512 <vivekanand80516@gmail.com>
2023-07-11 12:08:26 +0530

    changes on task profile

--- src/webparts/taskprofile/components/Taskprofile.tsx
+++ src/webparts/taskprofile/components/Taskprofile.tsx
@@ -1441,7 +1441,7 @@ export default class Taskprofile extends React.Component<ITaskprofileProps, ITas
                             </a>
 
 
-                            <div className="Footerimg d-flex align-items-center bg-fxdark justify-content-between p-2 ">
+                            <div className="Footerimg d-flex align-items-center bg-fxdark justify-content-between p-1 ">
                               <div className='usericons'>
                                 <span>
                                   <span >{imgData?.UploadeDate}</span>
@@ -1454,11 +1454,12 @@ export default class Taskprofile extends React.Component<ITaskprofileProps, ITas
                                 </span>
                               </div>
                               <div>
-                                <a className='images' target="_blank" data-interception="off" href={imgData?.ImageUrl}><span className='mx-2'><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M212.686 315.314L120 408l32.922 31.029c15.12 15.12 4.412 40.971-16.97 40.971h-112C10.697 480 0 469.255 0 456V344c0-21.382 25.803-32.09 40.922-16.971L72 360l92.686-92.686c6.248-6.248 16.379-6.248 22.627 0l25.373 25.373c6.249 6.248 6.249 16.378 0 22.627zm22.628-118.628L328 104l-32.922-31.029C279.958 57.851 290.666 32 312.048 32h112C437.303 32 448 42.745 448 56v112c0 21.382-25.803 32.09-40.922 16.971L376 152l-92.686 92.686c-6.248 6.248-16.379 6.248-22.627 0l-25.373-25.373c-6.249-6.248-6.249-16.378 0-22.627z"></path></svg></span></a>
+                               
                                 <span >
                                   {imgData?.ImageName?.length > 15 ? imgData?.ImageName.substring(0, 15) + '...' : imgData?.ImageName}
                                 </span>
                                 <span>|</span>
+                                <a className='images'  title="Expand Image"target="_blank" data-interception="off" href={imgData?.ImageUrl}><span className='mx-2'><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M212.686 315.314L120 408l32.922 31.029c15.12 15.12 4.412 40.971-16.97 40.971h-112C10.697 480 0 469.255 0 456V344c0-21.382 25.803-32.09 40.922-16.971L72 360l92.686-92.686c6.248-6.248 16.379-6.248 22.627 0l25.373 25.373c6.249 6.248 6.249 16.378 0 22.627zm22.628-118.628L328 104l-32.922-31.029C279.958 57.851 290.666 32 312.048 32h112C437.303 32 448 42.745 448 56v112c0 21.382-25.803 32.09-40.922 16.971L376 152l-92.686 92.686c-6.248 6.248-16.379 6.248-22.627 0l-25.373-25.373c-6.249-6.248-6.249-16.378 0-22.627z"></path></svg></span></a>
                               </div>
 
                             </div>
