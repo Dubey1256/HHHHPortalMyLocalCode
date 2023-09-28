e53df30e12660720d907c1d1c69d784a4c8899a5
@AnubhavS7 <anubhav@smalsus.com>
2023-09-28 12:22:47 +0530

    showtaskteammember

--- src/globalComponents/ShowTaskTeamMembers.tsx
+++ src/globalComponents/ShowTaskTeamMembers.tsx
@@ -1,20 +1,19 @@
 import * as React from "react";
 
-// import teamsImg from '../Assets/ICON/Teams-Logo.png'; 
-var siteUrl = ''
+// import teamsImg from '../Assets/ICON/Teams-Logo.png';
+var siteUrl = "";
 
 function ShowTaskTeamMembers(item: any) {
-
-  siteUrl = item.props?.siteUrl
+  siteUrl = item.props?.siteUrl;
   const [Display, setDisplay] = React.useState("none");
-  const [taskData, settaskData] = React.useState<any>()
+  const [taskData, settaskData] = React.useState<any>();
   let TaskUsers: any = [];
-//  let  taskDetails = item?.props;
+  //  let  taskDetails = item?.props;
   TaskUsers = item?.TaskUsers;
 
   React.useEffect(() => {
-    if(item?.props!=undefined){
-      let  taskDetails = item?.props;
+    if (item?.props != undefined) {
+      let taskDetails = item?.props;
       if (taskDetails["AssignedTo"] != undefined) {
         taskDetails["AssignedTo"]?.map((item: any, index: any) => {
           if (taskDetails?.TeamMembers != undefined) {
@@ -25,117 +24,203 @@ function ShowTaskTeamMembers(item: any) {
               }
             }
           }
-  
+
           item.workingMember = "activeimg";
-  
         });
       }
-      var array2: any = taskDetails["AssignedTo"] != undefined ? taskDetails["AssignedTo"] : [];
+      var array2: any =
+        taskDetails["AssignedTo"] != undefined ? taskDetails["AssignedTo"] : [];
       if (taskDetails["TeamMembers"] != undefined) {
-        taskDetails.array = array2.concat(taskDetails["TeamMembers"]?.filter((item: any) => array2?.Id != item?.Id))
+        taskDetails.array = array2.concat(
+          taskDetails["TeamMembers"]?.filter(
+            (item: any) => array2?.Id != item?.Id
+          )
+        );
       } else {
         taskDetails.array = array2;
       }
-  
-      taskDetails.TeamLeader = taskDetails["ResponsibleTeam"] != null ? GetUserObjectFromCollection(taskDetails["ResponsibleTeam"]) : null;
-  
-      taskDetails.TeamMembers = taskDetails?.array != null ? GetUserObjectFromCollection(taskDetails.array) : null;
-      settaskData(taskDetails)
+
+      taskDetails.TeamLeader =
+        taskDetails["ResponsibleTeam"] != null
+          ? GetUserObjectFromCollection(taskDetails["ResponsibleTeam"])
+          : null;
+
+      taskDetails.TeamMembers =
+        taskDetails?.array != null
+          ? GetUserObjectFromCollection(taskDetails.array)
+          : null;
+      settaskData(taskDetails);
     }
-    
-  }, [])
+  }, []);
   const GetUserObjectFromCollection = (UsersValues: any) => {
     let userDeatails = [];
     for (let index = 0; index < UsersValues?.length; index++) {
-      let senderObject:any = TaskUsers?.filter(function (user: any, i: any) {
+      let senderObject: any = TaskUsers?.filter(function (user: any, i: any) {
         if (user?.AssingedToUser != undefined) {
-          return user?.AssingedToUser["Id"] == UsersValues[index]?.Id
+          return user?.AssingedToUser["Id"] == UsersValues[index]?.Id;
         }
       });
       if (senderObject.length > 0) {
         userDeatails.push({
-          'Id': senderObject[0]?.AssingedToUser.Id,
-          'Name': senderObject[0]?.Email,
-          'Suffix': senderObject[0]?.Suffix,
-          'Title': senderObject[0]?.Title,
-          'userImage': senderObject[0]?.Item_x0020_Cover?.Url,
-          'activeimg2': UsersValues[index]?.workingMember ? UsersValues[index]?.workingMember : "",
-        })
+          Id: senderObject[0]?.AssingedToUser.Id,
+          Name: senderObject[0]?.Email,
+          Suffix: senderObject[0]?.Suffix,
+          Title: senderObject[0]?.Title,
+          userImage: senderObject[0]?.Item_x0020_Cover?.Url,
+          activeimg2: UsersValues[index]?.workingMember
+            ? UsersValues[index]?.workingMember
+            : ""
+        });
       }
-
     }
     return userDeatails;
-  }
+  };
   const handleSuffixHover = () => {
     //e.preventDefault();
-    setDisplay("block")
-
-  }
+    setDisplay("block");
+  };
   const handleuffixLeave = () => {
-    setDisplay("none")
-  }
-
-
+    setDisplay("none");
+  };
 
   return (
     <>
-      <div className='full-width'>
-
+      <div className="full-width">
         <div className="d-flex align-items-center">
-          {taskData?.TeamLeader != null && taskData?.TeamLeader?.length > 0 && taskData?.TeamLeader?.map((rcData: any, i: any) => {
-            return <div className="user_Member_img"><a href={`${taskData["siteUrl"]}/SitePages/TaskDashboard.aspx?UserId=${rcData?.Id}&Name=${rcData?.Title}`} target="_blank" data-interception="off" title={rcData?.Title}>
-              {rcData.userImage != null && <img className="workmember" src={rcData?.userImage}></img>}
-              {rcData.userImage == null && <span className="workmember bg-fxdark" >{rcData?.Suffix}</span>}
-            </a>
-            </div>
-          })}
-        {(taskData?.TeamLeader==null && taskData?.TeamLeader==undefined)&&(taskData?.TeamMembers != null && taskData?.TeamMembers?.length > 0) && <div className="user_Member_img">
-                <span className="workmember d-flex clearfix" ></span>
-                <span className="workmember bg-fxdark" ></span>
-        
-         </div>}
-
-          {taskData?.TeamMembers != null && taskData?.TeamMembers?.length > 0 &&
-            <div className="img  "><a href={`${taskData["siteUrl"]}/SitePages/TaskDashboard.aspx?UserId=${taskData?.TeamMembers[0]?.Id}&Name=${taskData?.TeamMembers[0]?.Title}`} target="_blank" data-interception="off" title={taskData?.TeamMembers[0]?.Title}>
-              {taskData?.TeamMembers[0].userImage != null && <img className={`workmember ${taskData?.TeamMembers[0].activeimg2}`} src={taskData?.TeamMembers[0]?.userImage}></img>}
-              {taskData?.TeamMembers[0].userImage == null && <span className={`workmember ${taskData?.TeamMembers[0].activeimg2}bg-fxdark border bg-e9 p-1 `} >{taskData?.TeamMembers[0]?.Suffix}</span>}
-            </a>
-            </div>
-          }
-
-          {taskData?.TeamMembers != null && taskData?.TeamMembers?.length == 2 && <div className="img mx-2"><a href={`${taskData["siteUrl"]}/SitePages/TaskDashboard.aspx?UserId=${taskData?.TeamMembers[1]?.Id}&Name=${taskData?.TeamMembers[1]?.Title}`} target="_blank" data-interception="off" title={taskData?.TeamMembers[1]?.Title}>
-            {taskData?.TeamMembers[1]?.userImage != null && <img className={`workmember ${taskData?.TeamMembers[1]?.activeimg2}`} src={taskData?.TeamMembers[1]?.userImage}></img>}
-            {taskData?.TeamMembers[1]?.userImage == null && <span className={`workmember ${taskData?.TeamMembers[1]?.activeimg2}bg-fxdark border bg-e9 p-1`} >{taskData?.TeamMembers[1]?.Suffix}</span>}
-          </a>
-          </div>
-          }
-          {taskData?.TeamMembers != null && taskData?.TeamMembers?.length > 2 &&
-            <div className="position-relative user_Member_img_suffix2 ms-1 alignCenter" onMouseOver={(e) => handleSuffixHover()} onMouseLeave={(e) => handleuffixLeave()}>+{taskData?.TeamMembers?.length - 1}
-              <span className="tooltiptext" style={{ display: Display, padding: '10px' }}>
-                <div>
-                  {taskData?.TeamMembers?.slice(1)?.map((rcData: any, i: any) => {
-
-                    return <div className=" mb-1 team_Members_Item" style={{ padding: '2px' }}>
-                      <a href={`${taskData["siteUrl"]}/SitePages/TaskDashboard.aspx?UserId=${rcData?.Id}&Name=${rcData?.Title}`} target="_blank" data-interception="off">
-
-                        {rcData?.userImage != null && <img className={`workmember ${rcData?.activeimg2}`} src={rcData?.userImage}></img>}
-                        {rcData?.userImage == null && <span className={`workmember ${rcData?.activeimg2}bg-fxdark border bg-e9 p-1`}>{rcData?.Suffix}</span>}
-
-                        <span className='mx-2'>{rcData?.Title}</span>
-                      </a>
-                    </div>
-
-                  })
-                  }
-
+          {taskData?.TeamLeader != null &&
+            taskData?.TeamLeader?.length > 0 &&
+            taskData?.TeamLeader?.map((rcData: any, i: any) => {
+              return (
+                <div className="user_Member_img">
+                  <a
+                    href={`${taskData["siteUrl"]}/SitePages/TaskDashboard.aspx?UserId=${rcData?.Id}&Name=${rcData?.Title}`}
+                    target="_blank"
+                    data-interception="off"
+                    title={rcData?.Title}
+                  >
+                    {rcData.userImage != null && (
+                      <img className="workmember" src={rcData?.userImage}></img>
+                    )}
+                    {rcData.userImage == null && (
+                      <span className="workmember bg-fxdark">
+                        {rcData?.Suffix}
+                      </span>
+                    )}
+                  </a>
                 </div>
-              </span>
-            </div>
-          }
+              );
+            })}
+          {taskData?.TeamLeader == null &&
+            taskData?.TeamLeader == undefined &&
+            taskData?.TeamMembers != null &&
+            taskData?.TeamMembers?.length > 0 && (
+              <div className="user_Member_img">
+                <span className="workmember d-flex clearfix"></span>
+                <span className="workmember bg-fxdark"></span>
+              </div>
+            )}
 
+          {taskData?.TeamMembers != null &&
+            taskData?.TeamMembers?.length > 0 && (
+              <div className="img  ">
+                <a
+                  href={`${taskData["siteUrl"]}/SitePages/TaskDashboard.aspx?UserId=${taskData?.TeamMembers[0]?.Id}&Name=${taskData?.TeamMembers[0]?.Title}`}
+                  target="_blank"
+                  data-interception="off"
+                  title={taskData?.TeamMembers[0]?.Title}
+                >
+                  {taskData?.TeamMembers[0].userImage != null && (
+                    <img
+                      className={`workmember ${taskData?.TeamMembers[0].activeimg2}`}
+                      src={taskData?.TeamMembers[0]?.userImage}
+                    ></img>
+                  )}
+                  {taskData?.TeamMembers[0].userImage == null && (
+                    <span
+                      className={`workmember ${taskData?.TeamMembers[0].activeimg2}bg-fxdark border bg-e9 p-1 `}
+                    >
+                      {taskData?.TeamMembers[0]?.Suffix}
+                    </span>
+                  )}
+                </a>
+              </div>
+            )}
+
+          {taskData?.TeamMembers != null &&
+            taskData?.TeamMembers?.length == 2 && (
+              <div className="img mx-2">
+                <a
+                  href={`${taskData["siteUrl"]}/SitePages/TaskDashboard.aspx?UserId=${taskData?.TeamMembers[1]?.Id}&Name=${taskData?.TeamMembers[1]?.Title}`}
+                  target="_blank"
+                  data-interception="off"
+                  title={taskData?.TeamMembers[1]?.Title}
+                >
+                  {taskData?.TeamMembers[1]?.userImage != null && (
+                    <img
+                      className={`workmember ${taskData?.TeamMembers[1]?.activeimg2}`}
+                      src={taskData?.TeamMembers[1]?.userImage}
+                    ></img>
+                  )}
+                  {taskData?.TeamMembers[1]?.userImage == null && (
+                    <span
+                      className={`workmember ${taskData?.TeamMembers[1]?.activeimg2}bg-fxdark border bg-e9 p-1`}
+                    >
+                      {taskData?.TeamMembers[1]?.Suffix}
+                    </span>
+                  )}
+                </a>
+              </div>
+            )}
+          {taskData?.TeamMembers != null &&
+            taskData?.TeamMembers?.length > 2 && (
+              <div
+                className="position-relative user_Member_img_suffix2 ms-1 alignCenter"
+                onMouseOver={(e) => handleSuffixHover()}
+                onMouseLeave={(e) => handleuffixLeave()}
+              >
+                +{taskData?.TeamMembers?.length - 1}
+                <span
+                  className="tooltiptext"
+                  style={{ display: Display, padding: "10px" }}
+                >
+                  <div>
+                    {taskData?.TeamMembers?.slice(1)?.map(
+                      (rcData: any, i: any) => {
+                        return (
+                          <div
+                            className=" mb-1 team_Members_Item"
+                            style={{ padding: "2px" }}
+                          >
+                            <a
+                              href={`${taskData["siteUrl"]}/SitePages/TaskDashboard.aspx?UserId=${rcData?.Id}&Name=${rcData?.Title}`}
+                              target="_blank"
+                              data-interception="off"
+                            >
+                              {rcData?.userImage != null && (
+                                <img
+                                  className={`workmember ${rcData?.activeimg2}`}
+                                  src={rcData?.userImage}
+                                ></img>
+                              )}
+                              {rcData?.userImage == null && (
+                                <span
+                                  className={`workmember ${rcData?.activeimg2}bg-fxdark border bg-e9 p-1`}
+                                >
+                                  {rcData?.Suffix}
+                                </span>
+                              )}
+
+                              <span className="mx-2">{rcData?.Title}</span>
+                            </a>
+                          </div>
+                        );
+                      }
+                    )}
+                  </div>
+                </span>
+              </div>
+            )}
         </div>
-
-
       </div>
     </>
   );
