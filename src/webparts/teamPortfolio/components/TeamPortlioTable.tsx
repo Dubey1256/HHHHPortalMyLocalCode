427736b33f7a9518f7e201ece30ad2c9de8e4c00
vivekanand2512 <vivekanand80516@gmail.com>
2023-10-12 13:16:04 +0530

    activity changes

--- src/webparts/teamPortfolio/components/TeamPortlioTable.tsx
+++ src/webparts/teamPortfolio/components/TeamPortlioTable.tsx
@@ -1952,15 +1952,19 @@ function TeamPortlioTable(SelectedProp: any) {
                 </div>
             </Panel>
             {isOpenActivity && (
-                <CreateActivity
-                    props={checkedList}
-                    Call={Call}
-                    TaskUsers={AllUsers}
-                    AllClientCategory={AllClientCategory}
-                    LoadAllSiteTasks={LoadAllSiteTasks}
-                    SelectedProp={SelectedProp}
-                    portfolioTypeData={portfolioTypeData}
-                ></CreateActivity>
+                <CreateActivity 
+          Call={Call}
+          AllListId={ContextValue}
+          TaskUsers={AllUsers}
+          AllClientCategory={AllClientCategory}
+          LoadAllSiteTasks={LoadAllSiteTasks}
+          selectedItem={
+            checkedList != null && checkedList?.Id != undefined
+              ? checkedList
+              : SelectedProp
+          }
+          portfolioTypeData={portfolioTypeData}
+        />
             )}
             {isOpenWorkstream && (
                 <CreateWS
