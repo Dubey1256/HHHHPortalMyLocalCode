669047d7f13464f0f0b7c47fd88f9cc3d2c7bc0f
@AnubhavS7 <anubhav@smalsus.com>
2023-10-13 19:12:59 +0530

    Restructuring

---
+++ src/webparts/siteDataBackupTool/components/ISiteDataBackupToolProps.ts
@@ -0,0 +1,9 @@
+export interface ISiteDataBackupToolProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+  SPBackupConfigListUrl: string
+  SPBackupConfigListID: string
+}
