c93891e3d7947768d1ea128f4b4e85bab62525d9
@AnubhavS7 <anubhav@smalsus.com>
2023-10-25 20:03:07 +0530

    hhhh

--- src/webparts/teamPortfolio/components/TeamPortlioTable.tsx
+++ src/webparts/teamPortfolio/components/TeamPortlioTable.tsx
@@ -13,10 +13,8 @@ import ShowTaskTeamMembers from "../../../globalComponents/ShowTaskTeamMembers";
 import { PortfolioStructureCreationCard } from "../../../globalComponents/tableControls/PortfolioStructureCreation";
 import CreateActivity from "../../servicePortfolio/components/CreateActivity";
 import CreateWS from "../../servicePortfolio/components/CreateWS";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Tooltip from "../../../globalComponents/Tooltip";
 import { ColumnDef } from "@tanstack/react-table";
-import "bootstrap/dist/css/bootstrap.min.css";
 import HighlightableCell from "../../../globalComponents/GroupByReactTableComponents/highlight";
 import Loader from "react-loader";
 import { Bars } from 'react-loader-spinner'
