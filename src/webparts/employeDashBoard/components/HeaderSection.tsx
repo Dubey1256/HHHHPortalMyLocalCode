41678a9f02159186ae7e0cd50014d57c09e91665
GuruCharan098 <anujarya1258@gmail.com>
2023-11-27 12:32:20 +0530

    documentsearch

--- src/webparts/employeDashBoard/components/HeaderSection.tsx
+++ src/webparts/employeDashBoard/components/HeaderSection.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import React, { useState } from 'react'
 // import TaskStatusTbl from './TaskStatusTbl';
 import { myContextValue } from '../../../globalComponents/globalCommon'
 import WorldClock from './WorldClock';
@@ -11,7 +12,8 @@ const Header = () => {
   const bottleneckTask: any = ContextData?.AlltaskData.BottleneckTask;
   const immediateTask: any = ContextData?.AlltaskData.ImmediateTask;
   const thisWeekTask: any = ContextData?.AlltaskData.ThisWeekTask;
-  const draftArray: any = ContextData?.AlltaskData?.DraftArray;
+  const draftArray: any = ContextData?.AlltaskData?.DraftCatogary;
+  const assignedTask: any = ContextData?.AlltaskData?.AssignedTask;
   const annouceMents: any = ContextData?.annouceMents;
   const [activeTile, setActiveTile] = useState('workingToday'); // Initialize the active tile
 
@@ -56,10 +58,11 @@ const Header = () => {
       </section>
       <section className="tabSec">
         <div className="d-flex justify-content-center">
-          <div className={`${activeTile === 'workingToday' ? 'col alignCenter me-3 mb-3 hreflink  p-3 empBg shadow-sm active empBg' : 'col alignCenter me-3 p-3 bg-white mb-3 hreflink shadow-sm'}`}
+          <div
+            className={`${activeTile === 'workingToday' ? 'col alignCenter me-3 mb-3 hreflink  p-3 empBg shadow-sm active empBg' : 'col alignCenter me-3 p-3 bg-white mb-3 hreflink shadow-sm'}`}
             onClick={() => handleTileClick('workingToday')}>
             <span className="iconSec" title='Working Today Task'>
-            {/* <span className="svg__iconbox svg__icon--calendarOne"></span> */}
+              {/* <span className="svg__iconbox svg__icon--calendarOne"></span> */}
               <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="#057BD0" className="bi bi-calendar4-event" viewBox="0 0 16 16" >
                 <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1H2zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5z" />
                 <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
@@ -75,15 +78,15 @@ const Header = () => {
                       <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z" />
                     </svg>
                   </span>
-                <span className='fw-semibold ms-1'>{todaysTask.length}</span></div>
+                  <span className='fw-semibold ms-1'>{todaysTask.length}</span></div>
                 <div className="mx-2">|</div>
                 <div className='alignCenter'>
-                <span title='Todays Time'>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#057BD0" className="bi bi-clock" viewBox="0 0 16 16">
+                  <span title='Todays Time'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#057BD0" className="bi bi-clock" viewBox="0 0 16 16">
                       <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
                       <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
-                </svg></span>
-                <span className='fw-semibold ms-1'>{currentTime}</span></div>
+                    </svg></span>
+                  <span className='fw-semibold ms-1'>{currentTime}</span></div>
               </div>
             </span>
           </div>
@@ -97,6 +100,18 @@ const Header = () => {
             <span className='ms-2'>
               <div>Working This Week</div>
               <div className="f-18 fw-semibold">{thisWeekTask.length}</div>
+            </span>
+          </div>
+          <div
+            className={`${activeTile === 'assignedTask' ? 'col alignCenter me-3 mb-3 hreflink  p-3 empBg shadow-sm active empBg' : 'col alignCenter me-3 p-3 bg-white hreflink  mb-3 shadow-sm'}`}
+            onClick={() => handleTileClick('assignedTask')}
+          >
+            <span className="iconSec">
+              <span title='Working This Week' className="svg__iconbox svg__icon--calendar"></span>
+            </span>
+            <span className='ms-2'>
+              <div>Assigned Task</div>
+              <div className="f-18 fw-semibold">{assignedTask.length}</div>
             </span>
           </div>
           <div
@@ -128,7 +143,7 @@ const Header = () => {
             className={`${activeTile === 'draft' ? 'col alignCenter hreflink  mb-3 p-3 empBg shadow-sm active empBg' : 'col alignCenter p-3 hreflink  bg-white mb-3 shadow-sm'}`}
             onClick={() => handleTileClick('draft')}>
             <span className="iconSec">
-              <span title='Draft Task' style={{width:'24px',height:'24px'}} className=" svg__iconbox svg__icon--draftOther"></span>
+              <span title='Draft Task' style={{ width: '24px', height: '24px' }} className=" svg__iconbox svg__icon--draftOther"></span>
             </span>
             <span className='ms-2'>
               <div>Draft Tasks</div>
@@ -138,77 +153,11 @@ const Header = () => {
         </div>
         {activeTile === 'workingToday' && <div><TaskStatusTbl activeTile={activeTile} /></div>}
         {activeTile === 'workingThisWeek' && <div><TaskStatusTbl activeTile={activeTile} /></div>}
+        {activeTile === 'assignedTask' && <div><TaskStatusTbl activeTile={activeTile} /></div>}
         {activeTile === 'bottleneck' && <div><TaskStatusTbl activeTile={activeTile} /></div>}
         {activeTile === 'immediate' && <div><TaskStatusTbl activeTile={activeTile} /></div>}
         {activeTile === 'draft' && <div><TaskStatusTbl activeTile={activeTile} /></div>}
       </section>
-      {/* <section className="tabSec">
-        <div className="d-flex justify-content-center">
-          <div className="col alignCenter me-3 p-3 mb-3 rounded active empBg shadow-sm">
-            <span className="iconSec">
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#057BD0" className="bi bi-calendar4-event" viewBox="0 0 16 16" >
-                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1H2zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5z" />
-                <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
-              </svg>
-            </span>
-            <span className="ms-2 tabText">
-              <div className="text white">Working Today Task </div>
-              <div className="tabResultText f-18">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16"  height="16" fill="#fff" className="bi bi-check2-circle" viewBox="0 0 16 16">
-                  <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z" />
-                  <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z" />
-                </svg>
-                {todaysTask.length}
-                <span className="mx-2">|</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" className="bi bi-clock" viewBox="0 0 16 16">
-                  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
-                  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
-                </svg>
-                {currentTime}
-              </div>
-            </span>
-          </div>
-          <div className="col alignCenter me-3 p-3 bg-white mb-3 rounded shadow-sm">
-            <span className="iconSec">
-              <span className="svg__iconbox svg__icon--calendar"></span>
-            </span>
-            <span className='ms-2'>
-              <div>Working This Week</div>
-              <div className="fw-bold f-18 tabResultText tabText">{thisWeekTask.length}</div>
-            </span>
-          </div>
-          <div className="col alignCenter me-3 mb-3 p-3 rounded shadow-sm">
-            <span className="iconSec">
-            <span className=" svg__iconbox svg__icon--bottleneck"></span>
-            </span>
-            <span className='ms-2'>
-              <div>Bottleneck Task </div>
-              <div className="fw-bold f-18 tabResultText tabText">
-                {bottleneckTask.length}
-              </div>
-            </span>
-          </div>
-          <div className="col alignCenter me-3 p-3 mb-3 rounded shadow-sm">
-            <span className="iconSec">
-              <span className=" svg__iconbox svg__icon--alert"></span>
-            </span>
-            <span className='ms-2'>
-              <div>Immediate Task</div>
-              <div className="fw-bold f-18 tabResultText tabText">{immediateTask.length}</div>
-            </span>
-          </div>
-          <div className="col alignCenter p-3 mb-3 rounded shadow-sm">
-            <span className="iconSec">
-              <span className=" svg__iconbox svg__icon--draft"></span>
-            </span>
-            <span className='ms-2'>
-              <div>Draft Task</div>
-              <div className="fw-bold f-18 tabResultText tabText">{draftArray?.length}</div>
-            </span>
-          </div>
-        </div>
-      </section> */}
-
     </div>
   );
 }
