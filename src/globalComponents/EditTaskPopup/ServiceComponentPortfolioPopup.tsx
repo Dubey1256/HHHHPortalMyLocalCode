e4a3cbbe693030aeec2c19a04e54ea7cd306c582
abhi98989 <abhishek09019@gmail.com>
2023-12-13 13:20:32 +0530

    column changes

--- src/globalComponents/EditTaskPopup/ServiceComponentPortfolioPopup.tsx
+++ src/globalComponents/EditTaskPopup/ServiceComponentPortfolioPopup.tsx
@@ -16,7 +16,7 @@ var LinkedServicesBackupArray: any = [];
 var MultiSelectedData: any = [];
 let AllMetadata: any = [];
 const ServiceComponentPortfolioPopup = ({ props, Dynamic, Call, ComponentType, selectionType, groupedData, showProject }: any) => {
-    const [modalIsOpen, setModalIsOpen] = React.useState(false);
+    const [modalIsOpen, setModalIsOpen] = React.useState(true);
     const [data, setData] = React.useState([]);
     const [CheckBoxData, setCheckBoxData] = React.useState([]);
     const [AllMetadataItems, setAllMetadataItems] = React.useState([]);
@@ -128,8 +128,8 @@ const ServiceComponentPortfolioPopup = ({ props, Dynamic, Call, ComponentType, s
                 setData(GlobalArray.ProjectData);
                 LinkedServicesBackupArray = GlobalArray.ProjectData;
             }
-            setModalIsOpen(true);
         }
+        setModalIsOpen(true);
     }
 
 
