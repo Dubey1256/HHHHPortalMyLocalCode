16512efa80e75ea98065315de2adf0fd7427abf4
PraveshReact <Praveshkmr825@gmail.com>
2023-12-19 16:18:19 +0530

    RecruitTool1

---
+++ src/webparts/recruitmentLandingPage/components/RecruitmentLandingPage.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import styles from './RecruitmentLandingPage.module.scss';
+import { IRecruitmentLandingPageProps } from './IRecruitmentLandingPageProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+import LandingPage from './LandingPage';
+
+export default class RecruitmentLandingPage extends React.Component<IRecruitmentLandingPageProps, {}> {
+  public render(): React.ReactElement<IRecruitmentLandingPageProps> {
+    const {   
+    } = this.props;
+
+    return (
+      <LandingPage props={this.props}></LandingPage>
+    );
+  }
+}
