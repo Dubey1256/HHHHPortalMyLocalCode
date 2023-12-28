489e5d724008c4ab9913ac5b014731b56907ae74
vivekanand2512 <vivekanand80516@gmail.com>
2023-12-28 18:58:27 +0530

    task

--- src/webparts/taskprofile/components/Taskprofile.tsx
+++ src/webparts/taskprofile/components/Taskprofile.tsx
@@ -2291,7 +2291,7 @@ class Taskprofile extends React.Component<ITaskprofileProps, ITaskprofileState> 
                                 }
                               </div>
                             </div>
-                            <div className="spxdropdown-menu ps-2 py-1">
+                            <div className="spxdropdown-menu ps-2 py-1" style={{zIndex:"0 !important"}}>
                               <span>Total Estimated Time : </span><span className="mx-1">{this.state.Result?.TotalEstimatedTime > 1 ? this.state.Result?.TotalEstimatedTime + " hours" : this.state.Result?.TotalEstimatedTime + " hour"} </span>
                             </div>
                           </div>
