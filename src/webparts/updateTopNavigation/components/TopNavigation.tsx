07cf9c6a28dd554dd398d77648e9579bc8960491
Santoshk00 <sk12211637@gmail.com>
2024-01-09 11:00:32 +0530

    ChnageEmailFormat

--- src/webparts/updateTopNavigation/components/TopNavigation.tsx
+++ src/webparts/updateTopNavigation/components/TopNavigation.tsx
@@ -128,6 +128,7 @@ const TopNavigation = (dynamicData: any) => {
       .get();
     console.log(TaskTypeItems);
     TaskTypeItems?.forEach((item: any) => {
+      item.Title = item?.Title.replace(/\b\w/g, (match:any) => match.toUpperCase());
       if (item.ownersonly == true) {
         item.image =`${dynamicData.dynamicData.siteUrl}/SiteCollectionImages/ICONS/24/Facilitators-do-not-disturb.png`;
       }
