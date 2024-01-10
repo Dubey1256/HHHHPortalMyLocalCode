4c36c7f514cffa60f1789df0d1a58e4d4e5b61cb
Anupam Rawat <anupamsingh.rawat@smalsus.com>
2024-01-10 11:59:08 +0530

    Recruitment tool and profile candidate

--- src/webparts/helloSpfx/components/InterviewFeedbackForm.tsx
+++ src/webparts/helloSpfx/components/InterviewFeedbackForm.tsx
@@ -38,6 +38,7 @@ interface RowData {
     Position: any;
     IsFavorite: boolean;
 }
+let overallRatings: any
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function InterviewFeedbackForm(props: any) {
     const [listData, setListData]: any = useState([]);
@@ -237,7 +238,7 @@ export default function InterviewFeedbackForm(props: any) {
                 let skills = JSON.parse(item?.SkillRatings)
                 let currentRatings = skills ? skills.reduce((sum: any, skill: any) => sum + (skill?.current || 0), 0) : 0;
                 let maxRatings = skills ? skills.reduce((sum: any, skill: any) => sum + (skill?.max || 0), 0) : 0;
-                let overallRatings = currentRatings != 0 || maxRatings != 0 ?  parseFloat(((currentRatings / maxRatings) * 10).toFixed(2)) : 0;
+                overallRatings = currentRatings != 0 || maxRatings != 0 ?  parseFloat(((currentRatings / maxRatings) * 5).toFixed(2)) : 0;
                 return {
                     ...item,
                     Position: item.Positions ? item.Positions.Title : null,
@@ -246,12 +247,13 @@ export default function InterviewFeedbackForm(props: any) {
                 };
             });
             const categorizedItems = response.reduce((accumulator: { newCandidates: any[]; inProcessCand: any[]; archiveCandidates: any[]; }, currentItem: {
-                Positions: any; Status0: any;
+                Positions: any; Status0: any; CandidateName: any
             }) => {
                 const itemWithPosition = {
                     ...currentItem,
                     Position: currentItem.Positions ? currentItem.Positions.Title : null,
-                    
+                    Title: currentItem.CandidateName,
+                    OverallRatings: overallRatings
                 };
 
                 switch (currentItem.Status0) {
