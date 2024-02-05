9153d10444e5bce55df49f41cd1ea06a967f47a7
ShivduttMishra <shivdutt@smalsus.com>
2024-02-05 11:12:13 +0530

    test

--- src/webparts/taskprofile/components/SmartTimeTotal.tsx
+++ src/webparts/taskprofile/components/SmartTimeTotal.tsx
@@ -255,13 +255,13 @@ let allTaskUsers:any;
                                             <tr className='for-c0l'>
                                                 <td style={{ width: "170px" }} colSpan={2}>
                                                     <div className='alignCenter'>
-                                                        <img className='workmember'  src={items?.AuthorImage != undefined && items?.AuthorImage !="" ? items?.AuthorImage:"https://hhhhteams.sharepoint.com/sites/HHHH/SiteCollectionImages/ICONS/32/icon_user.jpg"}></img>
+                                                        <img className='workmember'src={items?.AuthorImage != undefined && items?.AuthorImage !="" ? items?.AuthorImage:"https://hhhhteams.sharepoint.com/sites/HHHH/SiteCollectionImages/ICONS/32/icon_user.jpg"}></img>
                                                         <span className="ms-8">
                                                             <SmartTooltipComponent items={items}allTaskUsers={allTaskUsers}siteUrl={item?.AllListId?.siteUrl}/>
                                                         </span>
                                                     </div>
                                                 </td>
-                                                <td>Total Time{items.hoverTime.toFixed(2)}<span className='mx-1'>{items.hoverTime>1?'hours':'hour'}</span></td>
+                                                <td>Total Time {items.hoverTime.toFixed(2)}<span className='mx-1'>{items.hoverTime>1?'hours':'hour'}</span></td>
                                             </tr>
 
                                             {items?.additionaltime2?.length > 0 && items?.additionaltime2?.map((details: any) => {
