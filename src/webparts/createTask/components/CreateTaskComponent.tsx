9963a9593214b821eb52988683a4ce63a89fe85f
Smalsusanu1 <anubhav@smlasus.com>
2024-02-06 18:05:11 +0530

    Globalcommon

--- src/webparts/createTask/components/CreateTaskComponent.tsx
+++ src/webparts/createTask/components/CreateTaskComponent.tsx
@@ -1396,9 +1396,11 @@ function CreateTaskComponent(props: any) {
                                 >
                                     {row?.original?.Title}
                                 </a>
-                                {row?.original?.descriptionsSearch !== null && row?.original?.descriptionsSearch != '' && <InfoIconsToolTip Discription={row?.original?.descriptionsSearch} row={row?.original} />
-                                }
+                               
                             </span>
+                            <span className='mt--3'>
+                            {row?.original?.descriptionsSearch !== null && row?.original?.descriptionsSearch != '' && <InfoIconsToolTip Discription={row?.original?.descriptionsSearch} row={row?.original} />
+                                }</span>
                         </div>
                     </>
                 ),
