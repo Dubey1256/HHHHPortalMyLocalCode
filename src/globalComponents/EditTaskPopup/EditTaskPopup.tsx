df1ea7703bc51ba3bb29841fa0b53f0349aabe29
vivekanand2512 <vivekanand80516@gmail.com>
2024-03-27 18:54:34 +0530

    edittask popip

--- src/globalComponents/EditTaskPopup/EditTaskPopup.tsx
+++ src/globalComponents/EditTaskPopup/EditTaskPopup.tsx
@@ -2594,6 +2594,7 @@ const EditTaskPopup = (Items: any) => {
                             TaskDetailsFromCall[0].siteType = EditData.siteType;
                             TaskDetailsFromCall[0].siteUrl = siteUrls;
                             TaskDetailsFromCall[0].siteIcon = Items.Items.SiteIcon;
+                            TaskDetailsFromCall[0].PercentComplete = (TaskDetailsFromCall[0].PercentComplete * 100).toFixed(0);
                         }
                         let UpdatedDataObject: any = TaskDetailsFromCall[0]
                         let NewSmartPriority: any = globalCommon.calculateSmartPriority(UpdatedDataObject)
@@ -4603,6 +4604,7 @@ const EditTaskPopup = (Items: any) => {
             console.log("Bottleneck All Details:", copyWorkAction)
             setUseFor("")
         setApproverPopupStatus(false)
+        setApproverData([])
         }
    else{
     setApproverPopupStatus(false);
@@ -4622,9 +4624,11 @@ const EditTaskPopup = (Items: any) => {
     };
 
     const selectApproverFunction = (selectedData: any) => {
-        selectedData.Id = selectedData.AssingedToUserId;
-        setApproverData([...ApproverData, selectedData]);
-    };
+   let checkduplicateData :any=  ApproverData.filter((data:any)=>data?.AssingedToUserId!=selectedData?.AssingedToUserId)
+        if(checkduplicateData?.length==0){
+            setApproverData([...ApproverData, selectedData]);
+        }
+     };
 
 
 
@@ -4639,8 +4643,13 @@ const EditTaskPopup = (Items: any) => {
         if (type == "OnTaskPopup") {
             setApproverSearchKey(e.target.value);
         }
+        if (type =="OnPanel") {
+            setApproverSearchKey(e.target.value);
+        }
+     
         BottleneckSearchKey
         let tempArray: any = [];
+        
         if (searchedKey?.length > 0) {
             AllEmployeeData?.map((itemData: any) => {
                 if (itemData.Child != undefined && itemData.Child.length > 0) {
@@ -4927,6 +4936,7 @@ const EditTaskPopup = (Items: any) => {
         WorkingAction?.map((WAItemData: any, ItemIndex: number) => {
             if (WAItemData.Title == usefor && WAItemData?.InformationData?.length > 0) {
                 WAItemData?.InformationData?.map((item:any)=>{
+                    item.Id = item?.TaggedUsers?.AssingedToUserId;
                     selectedtagMember.push(item?.TaggedUsers)
                 })
               
@@ -5039,7 +5049,7 @@ const EditTaskPopup = (Items: any) => {
                         : "d-flex full-width pb-1"
                 }
             >
-                <div className="subheading siteColor"> {useFor!=""?`Select  ${useFor}`:`Select Approver`}</div>
+                <div className="subheading siteColor"> {useFor!=""?`Select${useFor}`:`Select Approver`}</div>
                 <Tooltip ComponentId="1683" isServiceTask={ServicesTaskCheck} />
             </div>
         );
