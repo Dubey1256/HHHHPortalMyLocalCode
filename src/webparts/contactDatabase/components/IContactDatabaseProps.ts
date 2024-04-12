d5699b26118a0f77ea4b2c7cf18ed8efc055d273
PraveshReact <Praveshkmr825@gmail.com>
2024-04-12 14:30:57 +0530

    contact

---
+++ src/webparts/contactDatabase/components/IContactDatabaseProps.ts
@@ -0,0 +1,10 @@
+export interface IContactDatabaseProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  Context: any;
+  userDisplayName: string;
+  TeamContactSearchlistIds:any;
+  TeamSmartMetadatalistIds:any;
+}
