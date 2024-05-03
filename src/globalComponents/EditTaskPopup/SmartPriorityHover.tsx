3f1c997ea75009916fd54bc94a4fba05aa2f16b0
Udbhav <udbhavsharma22>
2024-05-03 17:58:43 +0530

    Apply BODMAS RULE

--- src/globalComponents/EditTaskPopup/SmartPriorityHover.tsx
+++ src/globalComponents/EditTaskPopup/SmartPriorityHover.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 const SmartPriorityHover = (props: any) => {
     const checkImmedidate=props?.editValue?.TaskCategories?.some((cat: any) => cat.Title === 'Immediate');
-    const projectPriority=props?.editValue?.Project?.PriorityRank!=null?props?.editValue?.Project?.PriorityRank:0;
+    const projectPriority=props?.editValue?.Project?.PriorityRank!=null?props?.editValue?.Project?.PriorityRank:1;
     const taskPriorityValue=props?.editValue?.PriorityRank!=null?props?.editValue?.PriorityRank:0;
     return (
         <>
@@ -20,12 +20,13 @@ const SmartPriorityHover = (props: any) => {
             <div style={{color:'#ca4200'}}>ProjectPriority = {projectPriority}</div>
             {checkImmedidate==true?<div style={{color:'#b00000'}}>Immediate = 2</div>:''}
             <div>
-                <span className="siteColor">SmartPriority = </span>
-                (
+                <span className="siteColor">SmartPriority = </span>   
+                ((
                 <span style={{color:'#008600'}}>{taskPriorityValue}</span>
                   <span> {' '}+ {' '}</span>
+                  (
                   <span style={{color:'#ca4200'}}>{projectPriority}</span>
-                  {' '}*{' '}4){' '}/{' '} 5
+                  {' '}*{' '}4)){' '}/{' '} 5)
                   {checkImmedidate==true? <><span>{' '}  * {' '} </span> <span style={{color:'#b00000'}}> 2</span></> :''}   
                   <span>=</span>
                   <span className="siteColor boldClable">{' '} {props?.editValue?.SmartPriority}</span>    
