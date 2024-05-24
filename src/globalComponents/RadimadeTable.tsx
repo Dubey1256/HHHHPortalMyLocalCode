872db8f7a668db775941077dca4d1aed82f41bdb
vivekanand2512 <vivekanand80516@gmail.com>
2024-05-24 11:55:52 +0530

    radimade table

--- src/globalComponents/RadimadeTable.tsx
+++ src/globalComponents/RadimadeTable.tsx
@@ -129,22 +129,22 @@ function ReadyMadeTable(SelectedProp: any) {
     React.useEffect(() => {
         if (AllSiteTasksData?.length > 0) {
             if (isUpdated != "") {
-                if (portfolioTypeData.length > 0) {
-                    portfolioTypeData?.map((elem: any) => {
-                        if (elem.Title === isUpdated || isUpdated?.toLowerCase() === elem?.Title?.toLowerCase()) {
-                            portfolioColor = elem.Color;
-                        }
-                    })
-                }
+                // if (portfolioTypeData.length > 0) {
+                //     portfolioTypeData?.map((elem: any) => {
+                //         if (elem.Title === isUpdated || isUpdated?.toLowerCase() === elem?.Title?.toLowerCase()) {
+                //             portfolioColor = elem.Color;
+                //         }
+                //     })
+                // }
             } else {
-                if (portfolioTypeData.length > 0) {
-                    portfolioTypeData?.map((elem: any) => {
-                        if (elem.Title === "Component") {
-                            portfolioColor = elem.Color;
-                        }
-                    })
+                // if (portfolioTypeData.length > 0) {
+                //     portfolioTypeData?.map((elem: any) => {
+                //         if (elem.Title === "Component") {
+                //             portfolioColor = elem.Color;
+                //         }
+                //     })
 
-                }
+                // }
 
                 if (SelectedProp?.configration == "AllAwt" && SelectedProp?.SelectedItem != undefined) {
                     if ('Parent' in SelectedProp?.SelectedItem) {
