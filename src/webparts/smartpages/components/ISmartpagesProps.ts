b254729e6382bdb9fb5f6cfe3d6ca6b50c3d9ad5
Smalsusanu1 <114911362+Smalsusanu1@users.noreply.github.com>
2024-06-10 12:11:04 +0530

    EDitcomponet

--- src/webparts/smartpages/components/ISmartpagesProps.ts
+++
@@ -1,9 +0,0 @@
-export interface ISmartpagesProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-  SitesListUrl: string;
-  SmartMetadataListID: string;
-}
