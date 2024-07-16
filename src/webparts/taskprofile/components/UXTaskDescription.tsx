e5b50ab7f00f53f509fd3aa7c8082ff4630b59fc
vivekanand2512 <vivekanand80516@gmail.com>
2024-07-16 16:49:44 +0530

    task profile ux task desc compare setData

--- src/webparts/taskprofile/components/UXTaskDescription.tsx
+++ src/webparts/taskprofile/components/UXTaskDescription.tsx
@@ -1373,7 +1373,7 @@ const uxdescriptions = (props: any) => {
                                         )
                                     }
                                 })}
-                                 <a onClick={()=>setComparesetpannel(true)}>Compare</a>
+                                 <a className="ml-auto mt-2" onClick={()=>setComparesetpannel(true)}>Compare</a>
 
                             </ul>
 
