48b3dac91fc7e9f5e98fd8518d2537894f584a2b
Ayush Verma <112403149+AyushVe99@users.noreply.github.com>
2024-08-06 13:27:41 +0530

    Update CreateAllStructure.tsx

--- src/globalComponents/CreateAllStructure.tsx
+++ src/globalComponents/CreateAllStructure.tsx
@@ -711,7 +711,7 @@ const CreateAllStructureComponent = (props: any) => {
               <label>
                 <b>Select Portfolio Type</b>
               </label>
-              <div className="py-2">
+              <div className="py-3">
                 {props?.portfolioTypeData.map((item: any) => {
                   return (
                     <span>
@@ -1084,7 +1084,7 @@ const CreateAllStructureComponent = (props: any) => {
                                   }
                                 />
                                 
-                                {!(props?.SelectedItem?.Item_x0020_Type === "Component" && component.Feature.length === 1 && props?.SelectedItem === undefined)  && (
+                                {(props?.SelectedItem?.Item_x0020_Type === "Component" && component.Feature.length >= 1 && (component.SubComponent.length>=1 || component.Feature.length>1))  && (
                                   <span
                                     onClick={() =>
                                       handleDelete(
@@ -1098,7 +1098,7 @@ const CreateAllStructureComponent = (props: any) => {
                                     className="svg__iconbox svg__icon--trash"
                                   ></span>
                                 )}
-                                {component.SubComponent.length == 1 &&
+                                {component.Feature.length >= 1 &&
                                 ( props?.SelectedItem === undefined) && (
                                     <span
                                       onClick={() =>
